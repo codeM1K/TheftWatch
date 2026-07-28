e9ee100bde44c8d55ae0427fdf947332910978b3
Aris Alamanos <arialam80@gmail.com>
2026-07-28 15:09:49 +0300

    fix: component scanning for repos and Vaadin theme folder

--- src/main/java/com/theftwatch/theftwatch/config/TheftWatchApplication.java
+++ src/main/java/com/theftwatch/theftwatch/config/TheftWatchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.theftwatch")
 @EnableAsync
 @EnableScheduling
 @Theme(value = "theftwatch")
