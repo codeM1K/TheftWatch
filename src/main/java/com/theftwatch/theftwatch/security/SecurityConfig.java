dd87cad0b7716f63ede7b388c11fe96706302339
Aris Alamanos <arialam80@gmail.com>
2026-07-29 14:08:18 +0300

    fix: add AuthenticationManager bean to SecurityConfig

--- src/main/java/com/theftwatch/theftwatch/security/SecurityConfig.java
+++ src/main/java/com/theftwatch/theftwatch/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +17,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
